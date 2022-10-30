34eaf8cefba33df4e86189fc1215572a90676065
XS <xs2038@nyu.edu>
2022-10-30 19:44:34 -0300

    Countdown System Basic
    Made a basic countdown system like Minit. For unknown reason, TMPro not works so I used old Text object. It just contains a countdown and teleport and refill the timer after counts ends. Need further develop but enough for a demo.

---
+++ Assets/Scripts/CountDown.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CountDown : MonoBehaviour
+{
+    // A Demo Countdown Mechanic.Unfinished.
+    public bool isCounting = false;//Using another script to activate this.
+    public float counter;//Assessable for debug use. A timer from 0 to 60
+    //public TextMeshPro timerUI; TMPro not working for unknown reason
+    public Text timerUI;
+    public GameObject player;
+    
+    void Start()
+    {
+        isCounting = false;
+        counter = 0;
+    }
+
+    void Update()
+    {
+        if (isCounting)
+        {
+
+            int time = (int)(60-counter % 60);//Actural timer from 60 to 0
+
+            if (time>0)
+            {
+                counter += Time.deltaTime;
+            }
+            else
+            {
+                //TO DO: Add what happening when count down end
+                Debug.Log("Times Up!");
+                counter = 0;
+                player.transform.position = new Vector3(0,0,0);
+            }
+            timerUI.text = time.ToString();
+        }
+    }
+}
