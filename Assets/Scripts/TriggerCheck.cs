b43276adadfb70f92412ed5dce382d51faa32a8d
XS <xs2038@nyu.edu>
2022-11-10 22:27:56 -0400

    Sword Anim and Logic
    Made Sword animations and change some script to make attack check better

--- Assets/Scripts/TriggerCheck.cs
+++ Assets/Scripts/TriggerCheck.cs
@@ -5,25 +5,33 @@ using UnityEngine;
 public class TriggerCheck : MonoBehaviour
 {
     public List<GameObject> targets;
+    public GameObject tar;
     // Start is called before the first frame update
     void Start()
     {
-        List<GameObject> targets = new List<GameObject>();
+        // List<GameObject> targets = new List<GameObject>();
+        tar = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        targets.Add(collision.gameObject);
+       // targets.Add(collision.gameObject);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        tar = collision.gameObject;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        List<GameObject> targets = new List<GameObject>();
+        tar = null;
+        //List<GameObject> targets = new List<GameObject>();
     }
 }
