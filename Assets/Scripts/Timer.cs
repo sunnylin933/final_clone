038247bdbd96fb5583704d33f815f6f23aef2fcd
XS <xs2038@nyu.edu>
2022-11-30 11:16:31 -0400

    Change for output-available

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+//using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class Timer : MonoBehaviour
 {
